e11d9e45d6d8c6c6975dd953c3a18fb1dab1a414
Yuriy Borys <iam618384@gmail.com>
2020-11-10 20:49:09 +0200

    Basic project structure

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+const Homepage = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <section>
+                    <div>
+                        List
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default Homepage;
